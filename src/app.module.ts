218f5de113c8d6869422d0befff66331dd084fb7
utopia-code <utopia-code.developer@gmail.com>
2026-07-06 13:54:39 +0200

    CRM backend: install all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatalogModule } from './catalog/catalog.module';
+import { ClientModule } from './client/client.module';
+import { InteractionsModule } from './interactions/interactions.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'CRM-Galego',
+      autoLoadEntities: true,
+      synchronize: true, // SOLO desarrollo
+    }),
+    TasksModule,
+    InteractionsModule,
+    UsersModule,
+    ClientModule,
+    CatalogModule,
+    ScheduleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
